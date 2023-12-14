198411d89742e1c05e9bc1f040f9672cf8d98085
NigelOmondi <nigelomondi62@gmail.com>
2023-12-14 22:50:55 +0300

    Best band

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,8 +1,3 @@
--- Write a SQL script that lists all bands with Glam rock
--- as their main style, ranked by their longevity
-
-SELECT band_name,
-       IF(split = '0', 2022, split) - formed AS lifespan
-FROM metal_bands
-WHERE style LIKE '%Glam rock%'
-ORDER BY lifespan DESC;
+-- Lists all bands with Glam rock as their main style, ranked by their longevity
+SELECT band_name, (IFNULL(split, 2022) - formed)
+AS lifespan FROM metal_bands WHERE style LIKE '%Glam rock%' ORDER BY lifespan DESC;
