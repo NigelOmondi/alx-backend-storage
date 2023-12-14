4bf516b154f62de89a0bf7aac79e6803983c3368
NigelOmondi <nigelomondi62@gmail.com>
2023-12-14 22:56:00 +0300

    Average score

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,18 +1,17 @@
 -- Average score
 
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
 DELIMITER //
-
-CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
+CREATE PROCEDURE ComputeAverageScoreForUser(
+    IN _user_id INT
+)
 BEGIN
-    DECLARE avg_score FLOAT;
-
-    -- Calculate the average score for the user
-    SELECT AVG(score) INTO avg_score FROM corrections WHERE user_id = user_id;
-
-    -- Update the average score in the users table
-    UPDATE users SET average_score = avg_score WHERE id = user_id;
-END
-
-//
-
+    UPDATE users
+    SET average_score = (
+        SELECT AVG(score) 
+        FROM corrections
+        WHERE corrections.user_id = _user_id
+    )
+    WHERE id = _user_id;
+END //
 DELIMITER ;
